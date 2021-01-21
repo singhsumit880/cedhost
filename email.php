84b227937a55473325742f9e2c0944791450f2f2
sumit singh <singhsumit880@gmail.com>
2021-01-21 18:45:14 +0530

    payment

--- email.php
+++ email.php
@@ -1,15 +1,5 @@
 <?php
 
-/**
- * Php version 7.2.10
- *
- * @category Components
- * @package Packagename
- * @author Avinash Sharma <avimannu28@gmail.com>
- * @license http://www.php.net/license/3_01.txt PHP License 3.01
- * @link http://localhost/training/php%20mysql%20task1/register/signup.php
- */
-
 $email = $_POST['email'];
 $token = $_POST['token'];
 include_once './class/User.php';
